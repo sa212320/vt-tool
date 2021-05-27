5ad0ec8ca6318fa7fcb100635928aeada844463d
sa212320 <sa212320@gmail.com>
2021-05-27 10:06:11 +0800

    cron 分流

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,10 @@
 
 git fetch
 sudo forever stop server2.js
+sudo forever stop serverCron.js
 git reset --hard origin/main
 sudo npm run build
 sudo npm run export
 sudo forever start server2.js
+sudo forever start serverCron.js
 sudo forever list
