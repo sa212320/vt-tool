164201ecf9e74f34853f844202319397930bf2f3
sa212320 <sa212320@gmail.com>
2021-06-19 17:38:57 +0800

    f

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -16,7 +16,7 @@ global.searchVideos = [];
 global.videos = [];
 global.maxLiveVideoLength = 1;
 
-if (window?.location?.protocol) window.location.protocol = "https:";
+if (global?.location?.protocol) global.location.protocol = "https:";
 const MyApp = ({ Component, pageProps}) => {
   const [isMobile, setIsMobile] = useState();
   const [vtubers, setVtubers] = useState({});
